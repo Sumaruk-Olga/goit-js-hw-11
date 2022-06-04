45d3a31901e208c072e55fc7608691eb7924695c
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-06-05 01:23:22 +0300

    add coments

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ initPage();
 refs.form.addEventListener('input', debounce(onInputChange, DEBOUNCE_DELAY));
 refs.form.addEventListener('submit', onSubmitForm);
 
+
 function initPage() {
     //ховаємо непотрібну поки кнопку
     //elem.classList.add(cls) - додає клас cls до списку класів елемента.
@@ -51,6 +52,8 @@ function onInputChange(event) {
     localStorage.setItem(KEY_TO_LOCALSTORAGE, JSON.stringify(parsedSavedData));
 }
 
+
+
 function onSubmitForm(event) {
     event.preventDefault();
     // перевірка чи є данні
@@ -64,54 +67,23 @@ function onSubmitForm(event) {
     console.log('parsedSavedData', parsedSavedData);
 
     //і відправляємо запит на сервер
-    console.log('відправляємо запит на сервер');
+    console.log('відправляємо запит на сервер  за першою сторінкою');
     //після приходу відповіді малюємо інтерфейс
+
+
+    //показуємо кнопку і обробляємо запит ще, якщо треба
+    refs.more.classList.remove('visually-hidden');
+    refs.more.addEventListener('click', onClickSearch);
+
+
+    function onClickSearch(event) {
+        console.log('відправляємо запит на сервер  за наступною сторінкою');
+        //і домальовуємо інтерфейс
+}
 
     localStorage.removeItem(KEY_TO_LOCALSTORAGE);
     refs.form.reset();
+    //очищаємо контент сторінки і знову ховаємо кнопку
+
+
 }
-
-// Example
-// Performing a GET request
-
-// const axios = require('axios');
-
-// // Make a request for a user with a given ID
-// axios.get('/user?ID=12345')
-//   .then(function (response) {
-//     // handle success
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });
-
-// // Optionally the request above could also be done as
-// axios.get('/user', {
-//     params: {
-//       ID: 12345
-//     }
-//   })
-//   .then(function (response) {
-//     console.log(response);
-//   })
-//   .catch(function (error) {
-//     console.log(error);
-//   })
-//   .then(function () {
-//     // always executed
-//   });  
-
-// // Want to use async/await? Add the `async` keyword to your outer function/method.
-// async function getUser() {
-//   try {
-//     const response = await axios.get('/user?ID=12345');
-//     console.log(response);
-//   } catch (error) {
-//     console.error(error);
-//   }
-// }
