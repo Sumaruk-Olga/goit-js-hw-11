9ab5eab51dba437a1b358e42047e45b6f2e80e57
Olga Sumaruk <olga_sumaruk@hotmail.com>
2022-06-05 00:08:01 +0300

    create refs and add event listeners

--- src/index.js
+++ src/index.js
@@ -1,4 +1,67 @@
 import Notiflix from 'notiflix';
 import axios from "axios";
 
-// Notiflix.Notify.info('Too many ...');
+const refs = {
+    searchQuery: document.querySelector('input[name="searchQuery"]'),
+    submit:document.querySelector('button[type="submit"]'),
+};
+
+console.log(refs.searchQuery);
+console.log(refs.submit);
+
+refs.searchQuery.addEventListener('input', onInputChange);
+refs.submit.addEventListener('submit', onSubmitForm);
+
+
+function onInputChange(event) {
+    //
+}
+
+function onSubmitForm(event) {
+    event.preventDefault();
+}
+
+// Example
+// Performing a GET request
+
+// const axios = require('axios');
+
+// // Make a request for a user with a given ID
+// axios.get('/user?ID=12345')
+//   .then(function (response) {
+//     // handle success
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });
+
+// // Optionally the request above could also be done as
+// axios.get('/user', {
+//     params: {
+//       ID: 12345
+//     }
+//   })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .then(function () {
+//     // always executed
+//   });  
+
+// // Want to use async/await? Add the `async` keyword to your outer function/method.
+// async function getUser() {
+//   try {
+//     const response = await axios.get('/user?ID=12345');
+//     console.log(response);
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
